aeb47f759713b44cd62da8b61cdb259202b9d9ef
Karolina <karolina.suchom@gmail.com>
2020-03-06 21:59:46 +0100

    project creation

---
+++ nuxt.config.js
@@ -0,0 +1,61 @@
+
+export default {
+  mode: 'spa',
+  head: {
+    htmlAttrs: {
+      lang: 'pl'
+    },
+    title: process.env.npm_package_name || '',
+    meta: [
+      {
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1, user-scalable=no'
+      },
+      {
+        name: 'author',
+        content: 'Karolina Suchomska'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/images/favicon.ico'
+      }
+    ]
+  },
+  loading: {
+    color: '#fff'
+  },
+  manifest: {
+    icons: [
+      {
+        src: '/images/favicon.ico',
+        sizes: '192x192',
+        type: 'image/png'
+      }
+    ]
+  },
+  router: {
+    base: '/BatTowers/'
+  },
+  css: [
+    '@/assets/sass/main.scss'
+  ],
+  plugins: [
+  ],
+  modules: [
+    '@nuxtjs/axios'
+  ],
+  buildModules: [
+    '@nuxtjs/eslint-module'
+  ]
+}
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container">
+    <h1>
+      Bat Towers
+    </h1>
+  </div>
+</template>
