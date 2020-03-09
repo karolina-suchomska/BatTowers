a8f725f19917e1971655f356b58c480325670769
Karolina <karolina.suchom@gmail.com>
2020-03-09 19:48:20 +0100

    fixes style

--- components/Questionnaire.vue
+++ components/Questionnaire.vue
@@ -4,6 +4,7 @@
       {{ lang.questionnaireTitle }}
     </h2>
     <iframe
+      v-if="visible"
       :src="lang.questionnaire"
       frameborder="0"
     />
@@ -19,9 +20,35 @@
 
 <script>
 export default {
+  data () {
+    return {
+      visible: true
+    }
+  },
   computed: {
     lang () {
       return this.$store.state.lang.lang
+    }
+  },
+  watch: {
+    $route () {
+      this.readingWeight()
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.readingHeight)
+    this.readingHeight()
+  },
+  beforeDestroy () {
+    window.removeEventListener('scroll', this.readingHeight)
+  },
+  methods: {
+    readingHeight () {
+      if (window.innerWidth < 768) {
+        this.visible = false
+      } else {
+        this.visible = true
+      }
     }
   }
 }
